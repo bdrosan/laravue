84df58dfe5ac8fafca495b546a73f6b2d3eb6dcc
Md. Salah Uddin <30566025+bdrosan@users.noreply.github.com>
2022-02-13 01:42:16 +0600

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
