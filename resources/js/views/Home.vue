478d7d8fff29a20e523cd96744f42fec6d1c0b06
Md. Salah Uddin <30566025+bdrosan@users.noreply.github.com>
2022-02-14 00:59:05 +0600

    added sweeralert2

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div>Home</div>
+  <button @click="showAlert">Test SweetAlert</button>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    showAlert() {
+      // Use sweetalert2
+      this.$swal("Hello Vue world!!!");
+    },
+  },
+};
 </script>
 
 <style>
